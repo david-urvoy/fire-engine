1e98ccbbd523d020723e5ab72bc6dac1d302c527
David URVOY <dev@david.urvoy.email>
2025-08-19 12:09:12 +0200

    chore: tweak grid size

--- src/three/tools/extended-grid-helper.tsx
+++ src/three/tools/extended-grid-helper.tsx
@@ -1,13 +1,16 @@
 import { useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import type { Group } from 'three'
 import { game } from '../../game'
+import { useImperativeTweaks } from '../../ui/tweaks/imperative'
 
 function roundToNearestTens(num: number) {
 	return Math.round(num / 10) * 10
 }
 
+
 export function ExtendedGridHelper({ count = 5 }: { count?: number }) {
+	const [gridSize, setGridSize] = useState(12)
 	const groupRef = useRef<Group>(null)
 	useFrame(({ camera }) => {
 		const { x, z } = camera.position
@@ -15,16 +18,22 @@ export function ExtendedGridHelper({ count = 5 }: { count?: number }) {
 		const roundedZ = roundToNearestTens(z)
 		groupRef.current?.position.set(roundedX, 0, roundedZ)
 	})
+	useImperativeTweaks({
+		title: '𖣯 Grid', bindings: useCallback((folder) => [
+			folder.addBinding({ gridSize }, 'gridSize', { min: 2, max: 20, step: 2 })
+				.on('change', ({ value }) => setGridSize(value)),
+		], [])
+	})
 	return (
 		<group ref={groupRef} visible={game.isDebug}>
 			{Array.from({ length: count }, (_, i) =>
 				Array.from({ length: count }, (_, j) => {
-					const key = `grid ${i} ${j}`
+					const key = `g-${i}-${j}`
 					return (
 						<gridHelper
 							key={key}
-							args={[10, 10, 'red', 'green']}
-							position={[10 * (i - count * 0.5), 0, 10 * (j - count * 0.5)]}
+							args={[gridSize, gridSize, 'red', 'green']}
+							position={[gridSize * (i - count * 0.5), 0, gridSize * (j - count * 0.5)]}
 						/>
 					)
 				}),
