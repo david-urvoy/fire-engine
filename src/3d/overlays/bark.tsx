d5c545c00e294031e935aa34f379c59787815978
David URVOY <dev@david.urvoy.email>
2026-08-17 02:37:42 +0200

    fix: add visibility prop to Bark component for conditional rendering

--- src/3d/overlays/bark.tsx
+++ src/3d/overlays/bark.tsx
@@ -1,10 +1,10 @@
 import { Billboard, Float, Text } from '@react-three/drei'
 import { Suspense, type PropsWithChildren } from 'react'
 
-export function Bark({ children }: PropsWithChildren) {
+export function Bark({ children, visible }: PropsWithChildren<{ visible?: boolean }>) {
 	return (
 		<Suspense fallback={null}>
-			<Billboard>
+			<Billboard visible={visible}>
 				<Float speed={10} rotationIntensity={1} floatIntensity={0.5}>
 					<Text position-y={0.3} fontSize={0.1} color="white" anchorX="center" anchorY="middle">
 						{children}
